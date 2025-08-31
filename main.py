305dda6b69e10f69439269a11fdb327058343e19
Вова Дубинка <dubinkavovchik406@gmail.com>
2025-08-31 18:18:05 +0300

    add new endpoint, GET '/user/{id}'

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, HTTPException, status
+from db import crud
+
+app = FastAPI()
+
+@app.get("/user/{id}")
+def get_user(id: int):
+    user_db = crud.get_user_info(id)
+    if user_db is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Пользователь не найден")
+    return user_db
